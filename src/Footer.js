b422a5bf70d8ad9c49d8645b620fb1179371137d
Mariana Pereira <mariana.p.azevedopereira@gmail.com>
2023-08-22 22:31:58 +0100

    update footer with new github link

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <small>
       <a
-        href="https://github.com/Maryp90/Weather-App-SheCodes"
+        href="https://github.com/Maryp90/weather-react"
         target="_blank"
         className="githubLink"
         rel="noopener noreferrer"
