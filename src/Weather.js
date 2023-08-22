94410559347e2718f5fdead8dcb089910f8ff5ce
Mariana Pereira <mariana.p.azevedopereira@gmail.com>
2023-08-22 22:28:46 +0100

    added some line codes from codesandbox to vscode
    API mostly related

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: Math.round(response.data.temperature.current),
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: Math.round(response.data.wind.speed),
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    });
+  }
+
+  function submit(event) {
+    event.preventDefault();
+    let apiKey = "f3t5b36ab43852690f35359144ebo9d6";
+    let units = "metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+    <form onSubmit={submit}>
+      <input type="search" placeholder="Enter a city" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+
+function updateCity(event) {
+  setCity(event.target.value);
+}
+  
   return (
     <div className="Weather">
       <div className="row-custom space-between-center current-weather">
